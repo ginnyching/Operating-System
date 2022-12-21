95c27e64712ee8245f7031f3638191596373b941
ginnyching <ginny.ching@gmail.com>
2022-12-22 01:56:27 +0800

    hw1

---
+++ HW1/hw1_2.c
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <sys/wait.h>
+#include <unistd.h>
+
+using namespace std;
+
+int main()
+{
+	pid_t pid;
+	cout<<"Main Process ID : "<<getpid()<<endl;
+	
+	pid=fork();//fork1
+	
+	if(pid>0){
+		wait(NULL);
+		
+		pid=fork();//fork4
+		
+		if(pid>0){
+			wait(NULL);
+			pid=fork();//fork6
+			if(pid>0)
+				wait(NULL);
+			else if(pid==0){
+				cout<<"Fork 6. I'm the child "<<getpid()<<", my  parent is "<<getppid()<<"."<<endl;
+			}
+		}
+		
+		else if(pid==0){
+			cout<<"Fork 4. I'm the child "<<getpid()<<", my  parent is "<<getppid()<<"."<<endl;
+		}
+		
+		pid=fork();//fork5
+		
+		if(pid>0)
+			wait(NULL);
+		else if(pid==0){
+			cout<<"Fork 5. I'm the child "<<getpid()<<", my  parent is "<<getppid()<<"."<<endl;
+		}
+	}
+	
+	else if(pid==0){
+		cout<<"Fork 1. I'm the child "<<getpid()<<", my  parent is "<<getppid()<<"."<<endl;
+		
+		pid=fork();//fork2
+		
+		
+		if(pid>0){
+			wait(NULL);
+		}
+		else if(pid==0){
+			cout<<"Fork 2. I'm the child "<<getpid()<<", my  parent is "<<getppid()<<"."<<endl;
+			
+			pid=fork();//fork3
+			
+			if(pid>0){
+				wait(NULL);
+			}
+			else if(pid==0){
+				cout<<"Fork 3. I'm the child "<<getpid()<<", my  parent is "<<getppid()<<"."<<endl;
+			}
+		}
+		
+	}
+	return 0;
+}
